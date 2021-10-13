cfa43b001279b95d4fe664c42a054baedd2481fc
Sofia Tejada <79775413+sofiatejada@users.noreply.github.com>
2021-10-13 13:17:50 -0700

    New experiment (#13)
    * it works, maybe, yes?
    
    * app works
    
    * fix front end redirect
    
    * uncomment out deployed sites
    
    * fixed the route that goes to the continue page
    
    * final revision
    
    * fixing bug
    
    * nearly there

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS artists CASCADE;
 DROP TABLE IF EXISTS user_artists CASCADE;
+DROP TABLE IF EXISTS conversations CASCADE;
+DROP TABLE IF EXISTS user_conversations CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -12,10 +14,7 @@ CREATE TABLE users (
 
 CREATE TABLE artists (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  artist_name TEXT NOT NULL,
-  artist_image TEXT,
-  artist_link TEXT NOT NULL,
-  artist_genre TEXT
+  artists_array TEXT []
 );
 
 CREATE TABLE user_artists (
@@ -23,4 +22,15 @@ CREATE TABLE user_artists (
   artist_id BIGINT NOT NULL REFERENCES artists(id)
 );
 
--- CREATE TABLE 
+CREATE TABLE conversations (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  to_user BIGINT,
+  from_user BIGINT,
+  message TEXT,
+  date TEXT
+);
+
+CREATE TABLE user_conversations (
+  user_id BIGINT NOT NULL REFERENCES users(id),
+  conversation_id BIGINT NOT NULL REFERENCES conversations(id)
+);
