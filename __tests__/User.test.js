92bd4b3c2d46c698e62245dc22d5a3c0948613ea
Brianna Bias <brianna.bias@gmail.com>
2021-10-13 11:44:43 -0700

    all users test passing

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -25,4 +25,23 @@ describe('User routes', () => {
 
       expect(res.body).toEqual(sbeve);
   });
+    it('Gets all users via GET', async () => {
+        const sbeve = await User.insert({
+          displayName: 'Sbeve',
+          email: 'sbeve@busey.co',
+          profileURL: 'http://spotify.com',
+          image:
+            'https://upload.wikimedia.org/wikipedia/en/8/8b/ST3_Steve_Harrington_portrait.jpg',
+        });
+        const busey = await User.insert({
+          displayName: 'Gary Busey',
+          email: 'gary@busey.co',
+          profileURL: 'http://spotify.com',
+          image:
+            'https://images.mubicdn.net/images/cast_member/2321/cache-463-1602494874/image-w856.jpg',
+        });
+        const res = await request(app).get('/api/v1/users');
+
+        expect(res.body).toEqual([sbeve, busey]);
+    })
 });
