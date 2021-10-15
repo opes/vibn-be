1fef4655acbb8d4fc64cba287af940fff422ce45
Brianna Bias <brianna.bias@gmail.com>
2021-10-15 11:10:53 -0700

    cleanup

--- __tests__/Artist.test.js
+++ __tests__/Artist.test.js
@@ -37,38 +37,4 @@ describe('Artist routes', () => {
     expect(res.body).toEqual([GB, PTV]);
   });
 
-  it.skip('Finds a users Artist by ID', async () => {
-    const artist = await Artist.insert({
-      id: '4iJLPqClelZOBCBifm8Fzv',
-      artistName: 'Pierce The Veil',
-      artistImage:
-        'https://i.scdn.co/image/ab6761610000e5eb58c744a03131b95b9e846ff5',
-      artistUrl: 'https://open.spotify.com/artist/4iJLPqClelZOBCBifm8Fzv',
-      artistGenre: ['post-hardcore', 'hardcore', 'screamo'],
-    });
-
-    const user = await User.insert({
-      displayName: 'Sbeve',
-      email: 'sbeve@busey.co',
-      profileURL: 'http://spotify.com',
-      image:
-        'https://upload.wikimedia.org/wikipedia/en/8/8b/ST3_Steve_Harrington_portrait.jpg',
-    });
-
-    // const userArtist = {
-    //   userId: user.id,
-    //   userName: user.displayName,
-    //   userImageUrl: user.image,
-    //   artistId: artist.id,
-    //   artist: artist.artistName,
-    //   image: artist.artistImage,
-    //   link: artist.artistLink,
-    //   image: artist.artistImage,
-    // };
-
-    const res = await request(app)
-      .get(`/api/v1/users/${user.id}/artists/${artist.id}`);
-
-    expect(res.body).toEqual(user, artist);
-  });
 });
